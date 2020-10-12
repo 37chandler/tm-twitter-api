dbd2a3f0ccc52ef9c62e8052cd26b0a7e71eca26
jordanmcnea <54867347+jordanmcnea@users.noreply.github.com>
2020-10-12 11:48:46 -0600

    Add files via upload
    Created a notebook for various Tweepy functionality.

---
+++ API_keys.py
@@ -0,0 +1,4 @@
+key = 'qVwvijelkdfuhaljAxIEtPB3Tq'
+secret_key = 'M29LjXVBcZtDwthfiuh842R7Q20I0HhpQlchTFLCCy2wv7c9D'
+access = '419210344-gGwnP4ptrWZnkW5Ljfhaliu3FpsYOHCeo4kC73ZJ'
+secret_access = '4nY4DN4e6tcemfsadsiasliu3h4PGI0etrUKG3190U3ey'
