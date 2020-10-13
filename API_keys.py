3b85310942f1083fe9261cbefc7adc73fb3559b6
John Chandler <john.chandler@gmail.com>
2020-10-13 09:09:14 -0600

    mid-edit update

--- API_keys.py
+++ API_keys.py
@@ -1,4 +1,4 @@
-key = 'qVwvijelkdfuhaljAxIEtPB3Tq'
-secret_key = 'M29LjXVBcZtDwthfiuh842R7Q20I0HhpQlchTFLCCy2wv7c9D'
-access = '419210344-gGwnP4ptrWZnkW5Ljfhaliu3FpsYOHCeo4kC73ZJ'
-secret_access = '4nY4DN4e6tcemfsadsiasliu3h4PGI0etrUKG3190U3ey'
+api_key = 'qVwvijelkdfuhaljAxIEtPB3Tq'
+api_key_secret = 'M29LjXVBcZtDwthfiuh842R7Q20I0HhpQlchTFLCCy2wv7c9D'
+access_token = '419210344-gGwnP4ptrWZnkW5Ljfhaliu3FpsYOHCeo4kC73ZJ'
+access_token_secret = '4nY4DN4e6tcemfsadsiasliu3h4PGI0etrUKG3190U3ey'
